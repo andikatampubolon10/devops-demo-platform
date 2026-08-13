0b382f4222d3b304d8af941cf9a82b3a12e91162
Andika PT <andikatampubolon72@gmail.com>
2026-08-13 14:41:59 +0700

    test jenkins webhook

--- app/server.js
+++ app/server.js
@@ -34,7 +34,7 @@ async function publishOrder(order) {
 
 app.get("/", (req, res) => {
     res.json({
-        message: "DevOps Demo Plsatform dsadadsadais running!",
+        message: "DevOps Demo Plsatform dsadadsadais dsarunning!",
         status: "success"
     });
 });
