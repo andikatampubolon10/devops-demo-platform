761b470224bd6e24d90da76bc417a1b5cfd1a5b5
Andika PT <andikatampubolon72@gmail.com>
2026-08-13 15:28:36 +0700

    test github webhook

--- app/server.js
+++ app/server.js
@@ -34,7 +34,7 @@ async function publishOrder(order) {
 
 app.get("/", (req, res) => {
     res.json({
-        message: "DevOps Demo Plsatform dsadadsdsasdaddsadsadddasddadasdasaaadais dsarunning!",
+        message: "DevOps Demo Plsatfordsadm dsadadsdsasdaddsadsadddasddadasdasaaadais dsarunning!",
         status: "success"
     });
 });
