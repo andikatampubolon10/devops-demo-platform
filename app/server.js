28fc9ba116f832ca613d78e0084d661531b6f069
Andika PT <andikatampubolon72@gmail.com>
2026-08-21 11:37:02 +0700

    test production

--- app/server.js
+++ app/server.js
@@ -34,7 +34,7 @@ async function publishOrder(order) {
 
 app.get("/", (req, res) => {
     res.json({
-        message: "DevOps is running HOT RELOD FINAL 1234!",
+        message: "DevOps is running HOT RELOD FINAL 12!",
         status: "success"
     });
 });
